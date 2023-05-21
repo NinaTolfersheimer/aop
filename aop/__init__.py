a5d1b25903d63f85cd6de35d9e56a650bab35a00
Amélie Hohe <nina.tolfersheimer@posteo.de>
2023-05-21 19:46:26 +0200

    removing redundant information (requirements are already listed in requirements.txt)

--- aop/__init__.py
+++ aop/__init__.py
@@ -4,13 +4,5 @@
 About: This module provides the background functionality on which an
 Astronomical Observation Protocol Program can be built.
 
-Dependencies: This module was written and tested in Python 3.9.12.
-It depends on the following built-in libraries or library functions:
-    -> json
-    -> datetime.datetime
-    -> datetime.timezone
-    -> uuid.uuid4
-    -> os.makedirs
-Additionally, this module relies on the following third-party libraries:
-    -> astropy.time (v5.0.4)
+Third-party dependencies are listed in requirements.txt.
 """
