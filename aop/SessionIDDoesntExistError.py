adc9018b35111b5e088a818a5f6e9405a61132a8
Amélie Hohe <nina.tolfersheimer@posteo.de>
2023-05-23 21:28:46 +0200

    custom SessionIDDoesntExistError is raised upon trying to use a session_id that doesn't exist on the filepath provided

---
+++ aop/SessionIDDoesntExistError.py
@@ -0,0 +1,15 @@
+class SessionIDDoesntExistError(Exception):
+    """
+    An error raised upon providing a string to current_jd's time argument that is not interpretable as a time.
+    """
+
+    def __init__(self, invalid_id):
+        """
+        Initialization of a SessionIDDoesntExistError object
+
+        Parameters
+        ----------
+        invalid_id : str
+            the string causing the trouble
+        """
+        self.invalid_string = invalid_id
