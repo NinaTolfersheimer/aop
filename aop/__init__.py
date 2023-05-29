76c30b978897dcdfdfb180dfad7517e387858bd8
Amélie Hohe <nina.tolfersheimer@posteo.de>
2023-05-29 04:43:01 +0200

    reflect module changes to __init__.py

--- aop/__init__.py
+++ aop/__init__.py
@@ -1,18 +1,12 @@
 """
 @author: Amélie Solveigh Hohe
 
-About: This module provides the background functionality for an implementation of
+About: This package provides the background functionality for an implementation of
 the Astronomical Observation Protocol standard v1.0 (aop). It fully implements the
 standard, but is meant to be implemented by a front-end app.
 
 Third-party dependencies are listed in requirements.txt.
 """
 
-import aop.Session
-import aop.current_jd
-import aop.parse_session
-import aop.AopFileAlreadExistsError
-import aop.AolFileAlreadyExistsError
-import aop.AolNotFoundError
-import aop.SessionIDDoesntExistOnFilepathError
-import aop.InvalidTimeStringError
+import aop.aop
+import aop.tools
